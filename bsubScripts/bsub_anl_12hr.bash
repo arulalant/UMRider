5927034e58bf04d775fa08b578772f1edc93fdfa
arulalant <arulalant@gmail.com>
2016-01-12 16:07:42 +0530

    updated output; error files name

--- bsubScripts/bsub_anl_12hr.bash
+++ bsubScripts/bsub_anl_12hr.bash
@@ -5,8 +5,8 @@
 #BSUB -W 00:30                # wall-clock time (hrs:mins)
 #BSUB -n 4                    # number of tasks in job
 #BSUB -q small                # queue
-#BSUB -e errors.%J.hybrid     # error file name in which %J is replaced by the job ID
-#BSUB -o output.%J.hybrid     # output file name in which %J is replaced by the job ID
+#BSUB -e um2grb2.anl.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
+#BSUB -o um2grb2.anl.00hr.out.%J.hybrid     # output file name in which %J is replaced by the job ID
 
-/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/iGui/scripts/um2grb2_anl_12hr.py
+/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/UMRider/scripts/um2grb2_anl_12hr.py
 
