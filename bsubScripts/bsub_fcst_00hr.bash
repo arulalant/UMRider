44f652a138602f4f8f786e1c3974a2db5484e25f
arulalant <arulalant@gmail.com>
2016-01-14 18:54:33 +0530

    corrected err,out filename

--- bsubScripts/bsub_fcst_00hr.bash
+++ bsubScripts/bsub_fcst_00hr.bash
@@ -5,8 +5,8 @@
 #BSUB -W 00:30                # wall-clock time (hrs:mins)
 #BSUB -n 40                   # number of tasks in job
 #BSUB -q small             	  # queue
-#BSUB -e um2grb2.anl.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
-#BSUB -o um2grb2.anl.00hr.out.%J.hybrid     # output file name in which %J is replaced by the job ID
+#BSUB -e um2grb2.fcst.err.%J.hybrid     # error file name in which %J is replaced by the job ID
+#BSUB -o um2grb2.fcst.out.%J.hybrid     # output file name in which %J is replaced by the job ID
 
 /gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/UMRider/scripts/um2grb2_fcst_00hr.py
 
