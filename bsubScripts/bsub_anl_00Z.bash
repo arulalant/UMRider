c9d22521df17dc2862dee657f153c9bda5c1f8de
arulalant <arulalant@gmail.com>
2016-02-04 11:27:23 +0530

    increased bsub hours upto 6, essential when multiple dates are submitted for the job

--- bsubScripts/bsub_anl_00Z.bash
+++ bsubScripts/bsub_anl_00Z.bash
@@ -3,7 +3,7 @@
 #BSUB -L /bin/bash
 #BSUB -a poe                  # set parallel operating environment
 #BSUB -J um2grb2              # job name
-#BSUB -W 00:30                # wall-clock time (hrs:mins)
+#BSUB -W 06:00                # wall-clock time (hrs:mins)
 #BSUB -n 6                    # number of tasks in job
 #BSUB -q small                # queue
 #BSUB -e um2grb2.anl.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
