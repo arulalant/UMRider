145ed0da0da21db5f0df78cc7eab97b32666c143
arulalant <arulalant@gmail.com>
2016-02-05 20:25:35 +0530

    made it as generic path

--- bsubScripts/ncum_global_post/ncum_global_post_fcst_00Z.bash
+++ bsubScripts/ncum_global_post/ncum_global_post_fcst_00Z.bash
@@ -8,7 +8,28 @@
 #BSUB -e um2grb2.fcst.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
 #BSUB -o um2grb2.fcst.00hr.out.%J.hybrid     # output file name in which %J is replaced by the job ID
 
-export UMRIDER_SETUP=ncum_global_um2grb2_setup.cfg
-export UMRIDER_VARS=ncum_global_um2grb2_vars.cfg
-/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/UMRider/scripts/um2grb2_fcst_00Z.py
+# find out the directory of this bash script after submitted to bsub
+DIR="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )"
+
+# get the absolute path of the local table 
+localTable_relative_dir="$DIR/../../tables/local/ncmr/v1/"
+localTable_absolute_dir="$( cd "$localTable_relative_dir" && pwd )"
+localTable=$localTable_absolute_dir/ncmr_grib2_local_table
+
+# get the absolute path of the script for forecast 00utc
+g2scripts_relative_dir="$DIR/../../g2scripts/"
+g2scripts_absolute_dir="$( cd "$g2scripts_relative_dir" && pwd )"
+g2script=$g2scripts_absolute_dir/um2grb2_fcst_00Z.py
+
+# export the configure paths to needed variables
+export UMRIDER_SETUP=$DIR/ncum_global_um2grb2_setup.cfg
+export UMRIDER_VARS=$DIR/ncum_global_um2grb2_vars.cfg
+export GRIB2TABLE=$localTable
+
+echo "export UMRIDER_SETUP="$UMRIDER_SETUP
+echo "export UMRIDER_VARS="$UMRIDER_VARS
+echo "export GRIB2TABLE="$GRIB2TABLE
+
+# execute the script
+/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python $g2script
 
