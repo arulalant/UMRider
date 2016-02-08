b436d8182acafe008db74aac9b870973c01af3bf
arulalant <arulalant@gmail.com>
2016-02-08 19:43:45 +0530

    user defined file names; convert to grib1 option; grib2ctl option; g2ctl option

--- g2scripts/um2grb2_fcst_00Z.py
+++ g2scripts/um2grb2_fcst_00Z.py
@@ -14,7 +14,9 @@ import os, sys, datetime
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from loadconfigure import inPath, outPath, tmpPath, date, loadg2utils, \
                  debug, targetGridResolution, overwriteFiles, neededVars, \
-                 requiredLat, requiredLon, max_long_fcst_hours_at_00z
+                 requiredLat, requiredLon, max_long_fcst_hours_at_00z, \
+                 fcstOutGrib2FilesNameStructure, createGrib2CtlIdxFiles, \
+                 createGrib1CtlIdxFiles, convertGrib2FilestoGrib1Files
 
 if loadg2utils == 'system':
     # Load g2utils from system python which has installed through setup.py
@@ -33,36 +35,35 @@ elif loadg2utils == 'local':
 if isinstance(date, tuple):
     # Got tuple of date string.
     startdate, enddate = date
-    sDay = datetime.datetime.strptime(startdate, "%Y%m%d")
-    eDay = datetime.datetime.strptime(enddate, "%Y%m%d")
-    lag = datetime.timedelta(days=1)
     print "Got tuple dates"
-    print "So um2grb2 ana 00hr conversion - from %s to %s" % (startdate, enddate)
-    while sDay <= eDay:
-        # loop through until startdate incremented upto enddate
-        print "Going to start progress on", startdate
-        # call forecast conversion function w.r.t data assimilated 
-        # during long forecast hour - 00UTC.        
-        convertFcstFiles(inPath, outPath, tmpPath, 
-                        targetGridResolution=targetGridResolution, 
-                 date=startdate, utc='00', convertVars=neededVars, 
-                      latitude=requiredLat, longitude=requiredLon,
-                   max_long_fcst_hours=max_long_fcst_hours_at_00z,
-                           overwrite=overwriteFiles, lprint=debug)
-        print "Time lag incremented by 1"
-        sDay += lag
-        startdate = sDay.strftime('%Y%m%d')
-    # end of while sDay <= eDay:
-    print "Successfully completed all the dates till", enddate
+    print "So um2grb2 fcst 00Z conversion - from %s to %s" % (startdate, enddate)
 elif isinstance(date, str):
-    # only single date 
-    # call forecast conversion function w.r.t data assimilated 
-    # during long forecast hour - 00UTC.
-    print "um2grb2 fcst conversion - date", date
-    convertFcstFiles(inPath, outPath, tmpPath, 
-                        targetGridResolution=targetGridResolution, 
-                      date=date, utc='00', convertVars=neededVars, 
-                      latitude=requiredLat, longitude=requiredLon,
-                   max_long_fcst_hours=max_long_fcst_hours_at_00z,
-                           overwrite=overwriteFiles, lprint=debug)
+    # only single date
+    startdate, enddate = date, date
+    print "Got single string date"
+    print "So um2grb2 fcst 00Z conversion - on %s" % date
 # end of if isinstance(date, tuple):
+
+sDay = datetime.datetime.strptime(startdate, "%Y%m%d")
+eDay = datetime.datetime.strptime(enddate, "%Y%m%d")
+lag = datetime.timedelta(days=1)
+while sDay <= eDay:
+    # loop through until startdate incremented upto enddate
+    print "Going to start progress on", startdate
+    # call forecast conversion function w.r.t data assimilated 
+    # during long forecast hour - 00UTC.        
+    convertFcstFiles(inPath, outPath, tmpPath, 
+                    targetGridResolution=targetGridResolution, 
+             date=startdate, utc='00', convertVars=neededVars, 
+                  latitude=requiredLat, longitude=requiredLon,
+         fcstFileNameStructure=fcstOutGrib2FilesNameStructure, 
+                          createGrib2CtlIdxFiles=createGrib2CtlIdxFiles,
+             createGrib1CtlIdxFiles=createGrib1CtlIdxFiles,
+ convertGrib2FilestoGrib1Files=convertGrib2FilestoGrib1Files, 
+               max_long_fcst_hours=max_long_fcst_hours_at_00z,             
+                       overwrite=overwriteFiles, lprint=debug)
+    print "Time lag incremented by 1"
+    sDay += lag
+    startdate = sDay.strftime('%Y%m%d')
+# end of while sDay <= eDay:
+print "Successfully completed all the dates till", enddate
