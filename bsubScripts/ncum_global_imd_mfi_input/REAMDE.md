6610789b6c10ba842e83c7aca25ba7f49773bc7c
arulalant <arulalant@gmail.com>
2016-03-02 19:34:05 +0530

    imd mfi setup; call back script; bsub script

---
+++ bsubScripts/ncum_global_imd_mfi_input/REAMDE.md
@@ -0,0 +1,20 @@
+What is mean by MFI ??
+
+The IMD (Indian Meteorological Department) is in the middle of a massive integration exercise to get rid of its compartmentalised way of working and to adopt a more holistic and centralised model. The department has procured hardware systems that will facilitate this integration process. These systems will enable the department to get the weather data at the right time, with the required consistency and accuracy.
+
+The Met Department has ordered a 'Synergy system' from Toulose-based French company MFI (Meteo France International). The system will integrate the working environments of regional Met centres at Mumbai, Nagpur, Delhi, Chennai, Kolkatta and Guwhati which also includes other sub offices. Currently these centres work on their own models to process the data compiled from radar pictures, surface charts, synoptic charts and various other sources of data. This data is then sent in the form of bulletins to end users like the aviation sector, high sea travellers, ports, ONGC etc.
+
+Link : http://www.firstpost.com/business/biztech/business-tech/it-infrastructure/imds-it-integration-to-smoothen-up-weather-forecasting-1874249.html
+
+
+This will produce analysis 00utc files.
+
+$ bsub < ncum_global_imd_mfi_input_anl_00Z.bash
+
+This will produce forecast 00utc files.
+
+$ bsub < ncum_global_imd_mfi_input_fcst_00Z.bash
+
+
+Arulalan.T
+02-Mar-2016
---
+++ bsubScripts/ncum_global_imd_mfi_input/imd_mfi_rename_g2files_and_put_into_ftp.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+## This is call back script which will be executed after created the grib2 files.
+## 
+## IMD MFI System require grib2 filenames must follows their own nomenclature
+## In UMRider we can not create like that. So here we are just renaming the 
+## grib2 files as per their nomenclature!
+## 
+## And finally putting into their ftp server.
+
+## Arulalan.T
+## 02-Mar-2016.
+
+import os, sys, getopt
+
+
+def renameFiles(outpath):
+    cdir = os.getcwd()
+    os.chdir(outpath)
+    gfiles = [f for f in os.listdir(outpath) if f.endswith('.grb2')]
+    
+    mfi_name_structure = {'anal': 'YMXA00', 'fc186': 'YMXK00', 
+    'fc180': 'YMXK00', 'fc240': 'YMXK00', 'fc102': 'YMXK00', 'fc108': 'YMXK00',
+    'fc78': 'YMXK00', 'fc54': 'YMXX01', 'fc222': 'YMXK00', 'fc144': 'YMXK00',
+    'fc204': 'YMXK00', 'fc120': 'YMXK00', 'fc90': 'YMXK00', 'fc96': 'YMXK00', 
+    'fc168': 'YMXK00', 'fc228': 'YMXK00', 'fc126': 'YMXK00', 'fc12': 'YMXC00',
+    'fc36': 'YMXG00', 'fc30': 'YMXF00', 'fc18': 'YMXD00', 'fc198': 'YMXK00', 
+    'fc192': 'YMXK00', 'fc138': 'YMXK00', 'fc84': 'YMXK00', 'fc210': 'YMXK00',
+    'fc114': 'YMXK00', 'fc72': 'YMXK00', 'fc60': 'YMXJ00', 'fc234': 'YMXK00',
+    'fc174': 'YMXK00', 'fc42': 'YMXH00', 'fc216': 'YMXK00', 'fc132': 'YMXK00',
+    'fc48': 'YMXI00', 'fc156': 'YMXK00', 'fc162': 'YMXK00', 'fc150': 'YMXK00',
+     'fc24': 'YMXE00', 'fc06': 'YMXB00', 'fc66': 'YMXX02'}
+    
+    for gf in gfiles:
+        if not 'MFICODE' in gf: continue
+        mfi_g2out = gf.split('.grb2')[0]
+        mfi_g2out_name = mfi_g2out.split('_')
+        hr = mfi_g2out_name[-1].split('.')[0]
+        if not hr in mfi_name_structure: 
+            print "Couldnt get the IMD MFI name for hour ", hr 
+            continue
+        rval = mfi_name_structure.get(hr, 'MFICODE')
+        # updating RANDOM
+        mfi_g2out_name[1] = rval
+        mfi_g2out_name = '_'.join(mfi_g2out_name)
+        os.rename(gf, mfi_g2out_name)
+        print "renamed grib2 file from %s to %s" % (gf, mfi_g2out_name)
+    # end of for gf in gfiles:
+    os.chdir(cdir)
+# end of def renameFiles(outpath):
+
+if __name__ == '__main__':
+
+    date = None
+    outpath = None
+    oftype = None
+    utc = None
+    helpmsg = 'imd_mfi_rename_g2files_and_put_into_ftp.py --date=20160302 --outpath=path --oftype=analysis --utc=00'
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "d:o:t:z:", ["date=","outpath=", "oftype=", "utc="])
+    except getopt.GetoptError:
+        print helpmsg
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print helpmsg
+            sys.exit()
+        elif opt in ("-d", "--date"):
+            date = arg
+        elif opt in ("-o", "--outpath"):
+            outpath = arg 
+        elif opt in ("-t", "--oftype"):
+            oftype = arg
+        elif opt in ("-z", "--utc"):
+            utc = arg
+    # end of for opt, arg in opts:
+    
+    # pass the arg to function  
+    renameFiles(outpath)
