cf1b1dea10a8cea1b8fe0b1bac5c468f6f55c4c7
arulalant <arulalant@gmail.com>
2016-03-08 16:58:28 +0530

    added pressureLevels option

--- g2scripts/um2grb2_fcst_12Z.py
+++ g2scripts/um2grb2_fcst_12Z.py
@@ -18,7 +18,8 @@ from loadconfigure import inPath, outPath, tmpPath, date, loadg2utils, \
                    fcstOutGrib2FilesNameStructure, createGrib2CtlIdxFiles, \
                    createGrib1CtlIdxFiles, convertGrib2FilestoGrib1Files, \
                    start_step_long_fcst_hour, grib1FilesNameSuffix, \
-                   removeGrib2FilesAfterGrib1FilesCreated, callBackScript
+                   removeGrib2FilesAfterGrib1FilesCreated, pressureLevels, \
+                   callBackScript
 
 if loadg2utils == 'system':
     # Load g2utils from system python which has installed through setup.py
@@ -58,6 +59,7 @@ while sDay <= eDay:
                     targetGridResolution=targetGridResolution, 
              date=startdate, utc='12', convertVars=neededVars,
                   latitude=requiredLat, longitude=requiredLon, 
+                                pressureLevels=pressureLevels,
          fcstFileNameStructure=fcstOutGrib2FilesNameStructure, 
                 createGrib2CtlIdxFiles=createGrib2CtlIdxFiles,
                 createGrib1CtlIdxFiles=createGrib1CtlIdxFiles,
