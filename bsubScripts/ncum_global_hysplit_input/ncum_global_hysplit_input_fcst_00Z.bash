c97449ac012fbe3b2e3928ba04858c8407d1ee13
arulalant <arulalant@gmail.com>
2017-06-13 12:31:47 +0530

    update logpath

--- bsubScripts/ncum_global_hysplit_input/ncum_global_hysplit_input_fcst_00Z.bash
+++ bsubScripts/ncum_global_hysplit_input/ncum_global_hysplit_input_fcst_00Z.bash
@@ -7,8 +7,8 @@
 #BSUB -x                      # exclusive mode
 #BSUB -R span[ptile=16]       # task per node 
 #BSUB -q ultra             	  # queue
-#BSUB -e /gpfs3/home/umfcst/UMRiderLogs/hysplit/bsub/um2grb2.fcst.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
-#BSUB -o /gpfs3/home/umfcst/UMRiderLogs/hysplit/bsub/um2grb2.fcst.00hr.out.%J.hybrid     # output file name in which %J is replaced by the job ID
+#BSUB -e /gpfs3/home/umfcst/UMRiderLogs/hysplit/0p5/bsub/um2grb2.fcst.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
+#BSUB -o /gpfs3/home/umfcst/UMRiderLogs/hysplit/0p5/bsub/um2grb2.fcst.00hr.out.%J.hybrid     # output file name in which %J is replaced by the job ID
 
 # find out the directory of this bash script after submitted to bsub
 DIR="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )"
