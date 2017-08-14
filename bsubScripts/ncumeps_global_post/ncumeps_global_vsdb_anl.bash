65e79591fcb295bb998130c3a5e53a05cbff512e
arulalant <arulalant@gmail.com>
2017-08-14 13:15:55 +0530

    update

--- bsubScripts/ncumeps_global_post/ncumeps_global_vsdb_anl.bash
+++ bsubScripts/ncumeps_global_post/ncumeps_global_vsdb_anl.bash
@@ -7,9 +7,9 @@
 #BSUB -n 16                  # number of tasks in job
 #BSUB -x
 #BSUB -R span[ptile=16]
-#BSUB -q small             	  # queue
-#BSUB -e umeps2grb2.vsdb.err.%J.%I.hybrid     # error file name in which %J is replaced by the job ID
-#BSUB -o umeps2grb2.vsdb.out.%J.%I.hybrid     # output file name in which %J is replaced by the job ID
+#BSUB -q ensemble             	  # queue
+#BSUB -e /gpfs3/home/umeps/UMRiderLogs/vsdb/bsub/umeps2grb2.vsdb.err.%J.%I.hybrid     # error file name in which %J is replaced by the job ID
+#BSUB -o /gpfs3/home/umeps/UMRiderLogs/vsdb/bsub/umeps2grb2.vsdb.out.%J.%I.hybrid     # output file name in which %J is replaced by the job ID
 
 
 # find out the directory of this bash script after submitted to bsub
@@ -27,7 +27,8 @@ echo "export GRIB2TABLE="$GRIB2TABLE
 epsMeanScript=$DIR/ncumeps_create_memavg_vsdb_input.py
 
 export SHELL=/bin/bash
-export UMRIDER_STARTDATE=20170406
+#export UMRIDER_STARTDATE=20170406
+
 # set the hour to pass command line argument to create anl vsdb
 hour=0
 echo "hour="${hour}
