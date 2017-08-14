8252d7e22c8e2b27c934b8e126094c2150760d03
arulalant <arulalant@gmail.com>
2017-08-14 12:38:53 +0530

    added 75th hour

--- bsubScripts/ncum_regional_post/ncum_regional_post_fcst_00Z_Manual.bash
+++ bsubScripts/ncum_regional_post/ncum_regional_post_fcst_00Z_Manual.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 #BSUB -a poe                  # set parallel operating environment
-#BSUB -J ur2g2reg             # job name
+#BSUB -J u2g2reg[6-78:6]    # job name we need utp 78, then only it will extract upto 75 grib2.
 #BSUB -W 06:00                # wall-clock time (hrs:mins)
 #BSUB -n 16                   # number of tasks in job (max task in one node)
 #BSUB -x                      # exclusive mode
 #BSUB -R span[ptile=16]       # task per node 
 #BSUB -q ultra                # queue
-#BSUB -e /gpfs3/home/umfcst/UMRiderLogs/post/bsub/umreg2grb2.fcst.00hr.err.%J.hybrid     # error file name in which %J is replaced by the job ID
-#BSUB -o /gpfs3/home/umfcst/UMRiderLogs/post/bsub/umreg2grb2.fcst.00hr.out.%J.hybrid     # output file name in which %J is replaced by the job ID
+#BSUB -e /gpfs3/home/umfcst/UMRiderLogs/post/bsub/umreg2grb2.fcst.00hr.err.%J.%I.hybrid     # error file name in which %J is replaced by the job ID
+#BSUB -o /gpfs3/home/umfcst/UMRiderLogs/post/bsub/umreg2grb2.fcst.00hr.out.%J.%I.hybrid     # output file name in which %J is replaced by the job ID
 
 # find out the directory of this bash script after submitted to bsub
 DIR="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )"
@@ -35,8 +35,13 @@ echo "export UMRIDER_SETUP="$UMRIDER_SETUP
 echo "export UMRIDER_VARS="$UMRIDER_VARS
 echo "export GRIB2TABLE="$GRIB2TABLE
 
+export SHELL=/bin/bash
+# get the hour to pass command line argument (from based on JOB index)
+hour=$(printf "%02d" ${LSB_JOBINDEX})     # 2-digit number starting with 0
+hour0=$(expr $hour - 5) # yes, subtract 5 works correctly from 1 to 75 hours grib2 creation with 6hourly job submission.
+
 # sourcing umtid_bashrc to load module python-uvcdat-iris!
 source "$DIR/../umtid_bashrc"
 # execute the script
-python $g2script
+python $g2script --start_long_fcst_hour=${hour0} --end_long_fcst_hour=${hour}
 
