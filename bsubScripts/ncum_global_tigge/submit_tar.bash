a50bb83fb8c29c969dedfcc6afd7df9ba1e58099
arulalant <arulalant@gmail.com>
2017-08-14 13:16:26 +0530

    removed old files

--- bsubScripts/ncum_global_tigge/submit_tar.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export UMRIDER_STARTDATE=20170404
-
-
-NEPSJOBID=`bsub < tigge_tar_1to44mem_ftp.bash |tail -1| cut -d" " -f2 | tr -d "<" | tr -d ">"`
-
-bsub -w "done($NEPSJOBID)" < tigge_tar_ctl_ftp.bash
-
