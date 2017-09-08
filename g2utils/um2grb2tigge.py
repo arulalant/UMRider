df1de55b43e71cf0d1d6f2ae12e9be1e71ce77fe
arulalant <arulalant@gmail.com>
2017-09-08 12:08:49 +0530

    update snow varname

--- g2utils/um2grb2tigge.py
+++ g2utils/um2grb2tigge.py
@@ -388,7 +388,7 @@ ncumSTASH_tiggeVars = {
 ('time_integrated_toa_outgoing_longwave_flux', 'm01s02i205'): ('time_integrated_outgoing_long_wave_radiation', 'ttr', 'W m-2 s'),
 
 ('cloud_area_fraction_assuming_random_overlap', 'm01s09i216'): ('total_cloud_cover', 'tcc', '%'),
-('snowfall_amount', 'm01s00i023'): ('snow_depth_water_equivalent', 'sd', 'kg m-2')
+('surface_snow_amount_where_land', 'm01s00i023'): ('snow_depth_water_equivalent', 'sd', 'kg m-2')
 ######################## END OF TIGGE-VALID-VARS-IN-BOTH-NCUM-DETERMINISTIC-ENSEMBLES ################
 
 ### Doubts fluxes input needs to be divided by no of sec in the 3-hour or 1-hour 
@@ -1212,7 +1212,7 @@ def makeTotalCummulativeVars(arg):
     
     for infile in infiles:
         if not os.path.isfile(infile):
-            print "Error: The infile '%s' doesnt exist to calculate makeTotalCummulativeVars"
+            print "Error: The infile '%s' doesnt exist to calculate makeTotalCummulativeVars" % infile
         else:
             print "Exists: ", infile
             try:  
