450a878844e879bea27b3bb978b34bb7d7a58de6
arulalant <arulalant@gmail.com>
2017-10-09 17:01:21 +0530

    merging all vars in each members via cat cmd

--- bsubScripts/ncumeps_global_tigge/tigge_create_tarball_g2files_put_into_ftp-arulalan.py
+++ bsubScripts/ncumeps_global_tigge/tigge_create_tarball_g2files_put_into_ftp-arulalan.py
@@ -22,6 +22,16 @@ tigge_check = '/gpfs2/home/prasanna/SOFTWARE/GNU/grib_api-1.21.0-path/bin/tigge_
 
 filesCount = {'ttr': 41, 'lsm': 41, 'orog': 41, '10v': 41, 'tcc': 41, 'gh': 369, 'skt': 41, 'tp': 41, 'msl': 41, 'mx2t6': 40, '2d': 41, '10u': 41, 'mn2t6': 40, 'sshf': 41, 'slhf': 41, 'ssr': 41, '2t': 41, 'sp': 41, 'st': 41, 'q': 328, 'u': 328, 't': 328, 'str': 41, 'v': 328, 'sd': 41}
 
+dirsOrder = [
+    'gh', 'u', 'v', 'q', 't', '10u', '10v', '2t', 'mx2t6', 'mn2t6', 'skt', 'st', 
+    '2d', 'sp', 'msl', 'tp', 'ttr', 'lsm', 'tcc', 'slhf', 'ssr', 'sshf', 'str', 'sd', 'orog'
+]
+# merge cmd into single grib2 of each members
+catcmd = ['%s/z_tigge_c_dems*{0000..0240..6}*' % d for d in dirsOrder]
+catcmd = '    '.join(catcmd)
+catcmd = 'cat %s ' % catcmd
+catcmd += '  > %s'
+
 def createTarBalls(path, today, member):    
     
     member = str(member).zfill(3)
@@ -41,7 +51,7 @@ def createTarBalls(path, today, member):
     
     cdir = os.getcwd()
     os.chdir(inpath)
-    
+        
     for tgf in os.listdir('.'):
         cmd = tigge_check + '-v -w %s/*' % tgf
         tigge_check_val = os.system(cmd)  # it should return 0 on pass 
@@ -61,18 +71,28 @@ def createTarBalls(path, today, member):
     tardir = '../../TarFiles/%s' % today
     if not os.path.exists(tardir): os.makedirs(tardir)
     tarfile = 'ncmrwf_tigge_%s_%s.tar.gz' % (today, member)
+    mergedg2filepath = 'ncmrwf_dems_tigge_%s_%s.grib2' % (today, member)
     print "currnet path : ", os.getcwd()
     # normal "$ tar cvjf fcst_20160223.tar.bz2 *fcst*grb2" cmd takes 6 minutes 43 seconds.
     #
     # where as in parallel bz2, "$ tar -c *fcst*grb2 | pbzip2 -v -c -f -p32 -m500 > fcst_20160223_parallel.tar.bz2" cmd takes only just 23 seconds alone, with 32 processors and 500MB RAM memory.
-    #
-    # create analysis files tar file in parallel # -m500 need to be include for pbzip2
-    tg_files = '  '.join([' -C %s . ' % os.path.join(inpath, tgf) for tgf in os.listdir('.')])
+    #    
+        
+    catcmd_out = catcmd % mergedg2filepath
+    subprocess.call(catcmd_out, shell=True)
+    time.sleep(30)
     
-    cmd = "tar -c  %s | %s -v  -c -f -p32 > %s/%s" % (tg_files, pigz, tardir, tarfile)
+    gzip_cmd = '%s -9 -p 32 %s' % (pigz, mergedg2filepath)
+    subprocess.call(gzip_cmd, shell=True)
+    time.sleep(5)
     
-    print cmd
-    subprocess.call(cmd, shell=True)
+#    # create analysis files tar file in parallel # -m500 need to be include for pbzip2
+#    tg_files = '  '.join([' -C %s . ' % os.path.join(inpath, tgf) for tgf in os.listdir('.')])
+#    
+#    cmd = "tar -c  %s | %s -v  -c -f -p32 > %s/%s" % (tg_files, pigz, tardir, tarfile)
+#    
+#    print cmd
+#    subprocess.call(cmd, shell=True)
          
     if member == '000':
         tarpath = os.path.abspath(tardir)        
