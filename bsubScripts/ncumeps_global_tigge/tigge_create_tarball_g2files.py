c7ab8a86f2e74f87aaddbb6ec6bd337460b329e4
arulalant <arulalant@gmail.com>
2017-10-20 12:32:05 +0530

    update comment

--- bsubScripts/ncumeps_global_tigge/tigge_create_tarball_g2files.py
+++ bsubScripts/ncumeps_global_tigge/tigge_create_tarball_g2files.py
@@ -14,7 +14,7 @@
 ## Arulalan.T
 ## 04-Mar-2016.
 
-import os, subprocess, datetime, getopt, sys, glob, time 
+import os, subprocess, datetime, getopt, sys, glob, time
 
 pbzip2 = '/gpfs1/home/Libs/GNU/ZIPUTIL/pbzip2'
 pigz = '/gpfs1/home/Libs/GNU/ZIPUTIL/pigz'
@@ -84,15 +84,16 @@ def createTarBalls(path, today, member):
     print "gzip_cmd = ", gzip_cmd
     subprocess.call(gzip_cmd, shell=True)
     time.sleep(5)
-    
-    if member == '000':
-        # remove yesterday directory!!!
+    print os.getcwd(), member    
+    if member == '000':        
+        # remove today directory!!!
+        print "path",  path
         if os.path.exists(path):    
             cmd = "rm -rf %s" % path
             print cmd
             subprocess.call(cmd, shell=True)
-        # end of if os.path.exists(path): 
-     # end of if member == '000':
+         # end of if os.path.exists(yDayPath):     
+    # end of if member == '000': 
     os.chdir(cdir)  
 # end of def createTarBalls(path, today, ...):
 
